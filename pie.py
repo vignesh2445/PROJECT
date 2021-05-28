52f67846ee8fe3a75020454f8a4c842e7c86a749
vignesh2445 <68011720+vignesh2445@users.noreply.github.com>
2021-05-28 19:04:14 +0530

    Add files via upload

---
+++ pie.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+vigneshwaran=np.array([25,25,25,15])
+mytechnicalskills =["python","java","C","HTML"]
+myexplode = [0.2,0,0,0]
+
+plt.pie(vigneshwaran,labels =mytechnicalskills ,explode=myexplode,shadow=True)
+plt.show()
